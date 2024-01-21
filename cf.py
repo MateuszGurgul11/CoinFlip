d37754b809bc62cbbd93a40c94e17c44b088593c
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-01-21 18:19:45 +0100

    Program losuje miedzy orlem a reszka

--- cf.py
+++ cf.py
@@ -1,1 +1,18 @@
-print('1')
+import random
+
+def CoinFlip():
+    coin = ['Orzeł', 'Reszka']
+    coinFlip = random.choice(coin)
+
+    print(coinFlip)
+
+def restart():
+    while True:
+        CoinFlip()
+        answer = input("Czy chcesz kontynuowac [T/N] ")
+        if(answer.upper() != 'T'):
+            break
+
+
+
+restart()
